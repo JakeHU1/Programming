b40e98aa13c2ade2abcf0e550cebcd890bd22201
Jake <jake.vanbiezen@student.hu.nl>
2018-09-16 12:56:01 +0200

    Eerste test

---
+++ Starten met Python (les 2)/4. Boolean Expressions.py
@@ -0,0 +1,18 @@
+#opdracht 1
+a = 6
+b = 7
+print(75 > a and 75 < b)
+
+#opdracht 2
+voornaam = 'Jake'
+tussenvoegsel = 'van'
+achternaam = 'Biezen'
+mijnnaam = voornaam + ' ' + tussenvoegsel + ' ' + achternaam
+print(len(mijnnaam) == len(voornaam) + len(tussenvoegsel) + len(achternaam))
+
+#opdracht 3
+print(len(mijnnaam) > 5*len(tussenvoegsel))
+
+#opdracht 4
+print(tussenvoegsel in achternaam)
+
