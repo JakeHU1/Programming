5c34963782283fcad8921d04f47fdec1fd05306d
Jake <jake.vanbiezen@student.hu.nl>
2018-09-16 12:40:05 +0200

    Eerste test

---
+++ Starten met Python (les 2)/2. Strings.py
@@ -0,0 +1,18 @@
+#opdracht 1
+word = 'Supercalifragilisticexpialidocious'
+print(len(word))
+
+#opdracht 2
+word = 'Supercalifragilisticexpialidocious'
+print('ice' in word)
+
+#opdracht 3
+word1 = 'Antidisestablishmentarianism'
+word2 = 'Honorificabilitudinitatibus'
+print(len(word1)>len(word2))
+
+
+#opdracht 4
+names = ['Berlioz', 'Berodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+names.sort()
+print(names[0] + 'is first, ' + names[-1] + ' is last')
