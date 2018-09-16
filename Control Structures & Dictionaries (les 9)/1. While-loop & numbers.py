2aa853ed26866419541a24e19d04d6dffb257f2f
Jake <jake.vanbiezen@student.hu.nl>
2018-09-16 14:41:42 +0200

    Eerste test

--- Control Structures & Dictionaries (les 9)/1. While-loop & numbers.py
+++ Control Structures & Dictionaries (les 9)/1. While-loop & numbers.py
@@ -1,1 +1,9 @@
-print(x)
+som, getallen = 0, 0
+while True:
+    getal = input('Geef een getal: ')
+    if int(getal) == 0:
+        print('er zijn ' + str(getallen) + ' ingevoerd, de som is: ' + str(som))
+        break
+    else:
+        som += int(getal)
+        getallen += 1
