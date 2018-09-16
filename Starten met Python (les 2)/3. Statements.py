51233da3e5b8a447d7a3a61d86c062d0b3565e9d
Jake <jake.vanbiezen@student.hu.nl>
2018-09-16 12:47:40 +0200

    Eerste test

---
+++ Starten met Python (les 2)/3. Statements.py
@@ -0,0 +1,14 @@
+#opdracht 1
+a = 6
+b = 7
+
+#opdracht 2
+c = (a+b)/2
+
+#opdracht 3
+voornaam = 'Jake'
+tussenvoegsel = 'van'
+achternaam = 'Biezen'
+
+#opdracht 4
+mijnnaam = voornaam + ' ' + tussenvoegsel + ' ' + achternaam
